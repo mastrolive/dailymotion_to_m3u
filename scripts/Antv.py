f9e541f8d36cb50fac645e5e8cb6fc25cb6209e9
Online24 <56545941+mastrolive@users.noreply.github.com>
2023-02-27 01:40:09 +0800

    Update Antv.py

--- scripts/Antv.py
+++ scripts/Antv.py
@@ -1,5 +1,16 @@
 #! /usr/bin/python3
 
+banner = r'''
+#########################################################################
+#      ____            _           _   __  __                           #
+#     |  _ \ _ __ ___ (_) ___  ___| |_|  \/  | ___   ___  ___  ___      #
+#     | |_) | '__/ _ \| |/ _ \/ __| __| |\/| |/ _ \ / _ \/ __|/ _ \     #
+#     |  __/| | | (_) | |  __/ (__| |_| |  | | (_) | (_) \__ \  __/     #
+#     |_|   |_|  \___// |\___|\___|\__|_|  |_|\___/ \___/|___/\___|     #
+#                   |__/                                                #
+#                                  >> https://github.com/benmoose39     #
+#########################################################################
+'''
 
 import requests
 import os
@@ -18,7 +29,7 @@ def grab(line):
         _id = line.split('/')[4]
         response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}', proxies=proxies).json()['qualities']['auto'][0]['url']
         m3u = s.get(response, proxies=proxies).text
-        m3u = m3u8.strip().split('\n')[1:]
+        m3u = m3u.strip().split('\n')[1:]
         d = {}
         cnd = True
         for item in m3u:
@@ -29,12 +40,12 @@ def grab(line):
             else:
                 d[resolution]= item
             cnd = not cnd
-        #print(m3u8)
+        #print(m3u)
         m3u = d[max(d, key=int)]    
     except Exception as e:
         m3u = na
     finally:
-        print(m3u8)
+        print(m3u)
 
 print('#EXTM3U')
 print(banner)
